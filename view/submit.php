12a7dba4004fa665d0831558b7305c1a9ee70101
Jason Alcantara <38396215+jason-alcantara@users.noreply.github.com>
2019-11-04 22:16:56 +0900

    Added program files

---
+++ view/submit.php
@@ -0,0 +1,17 @@
+<?php
+    require "header.php";
+?>
+
+        <main>
+            <div class="signup-container d-flex align-items-center justify-content-center">
+                <div class="done-form text-center border rounded bg-light">
+                    <h2 class="font-weight-bold mb-4">完了しました</h2>
+                    <p class="message mb-4">登録の確認としてメールが送信されました。</br>どうもありがとうございました。</p>
+                    <button class="btn btn-success"><a href="./">ホームへ</a></button>
+                </div>
+            </div>
+        </main>
+
+<?php
+    require "footer.php";
+?>
